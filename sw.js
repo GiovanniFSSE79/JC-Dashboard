f815ddddfec03a6cf36c77cc8c930ff3f025a931
GiovanniFSSE79 <GiovanniFSSE79@users.noreply.github.com>
2026-04-20 22:20:35 -0300

    JC Dashboard v7 - Vercel ready

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'jc-dashboard-v7';
+const urlsToCache = [
+  './JC_Dashboard_v7.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
